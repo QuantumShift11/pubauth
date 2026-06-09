bb370981b2f20d4ba984af8694462fcf032dc2e1
QuantumShift11 <33118675+QuantumShift11@users.noreply.github.com>
2026-06-09 22:34:24 +0530

    Wire gateway service to HTTP adapter

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,5 +1,20 @@
+import { loadConfig } from '../../../packages/config/src/index.js';
+import { createLogger } from '../../../packages/logger/src/index.js';
+import { startNodeServer } from '../../../packages/http/src/index.js';
+import { buildGatewayRoutes } from './routes.js';
+
 export async function startGatewayService(): Promise<void> {
-  console.log('PubAuth Gateway service starting');
+  const config = loadConfig('gateway');
+  const logger = createLogger('gateway');
+
+  startNodeServer({
+    port: config.port,
+    routes: buildGatewayRoutes(),
+  });
+
+  logger.info('gateway_service_started', {
+    port: config.port,
+  });
 }
 
 startGatewayService().catch((error) => {
